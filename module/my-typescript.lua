046afee1bf156c374f27fd5a1ee60db307e08e4f
Hwlhwlxyz <huweilun1994@hotmail.com>
2025-05-19 16:47:05 -0400

    add dprint format

--- module/my-typescript.lua
+++ module/my-typescript.lua
@@ -11,6 +11,42 @@ local function setup_filetype_indent()
     })
 end
 setup_filetype_indent()
+
+local function run_dprint_format()
+	local filepath = vim.fn.expand("%:p")
+	  vim.cmd("write")
+	  vim.fn.jobstart({ "dprint", "fmt", filepath }, {
+		on_exit = function()
+		  vim.cmd("silent! edit")
+		end,
+	  })
+end
+local dprint_autoformat_enabled = {}
+vim.api.nvim_create_user_command("DprintFormat", function(opts)
+    local buf = vim.api.nvim_get_current_buf()
+
+  if opts.args == "toggle" then
+    if dprint_autoformat_enabled[buf] then
+      dprint_autoformat_enabled[buf] = false
+      vim.api.nvim_clear_autocmds({ group = "DprintAutoFormat", buffer = buf })
+      vim.notify("dprint autoformat disabled", vim.log.levels.INFO)
+    else
+      dprint_autoformat_enabled[buf] = true
+      vim.api.nvim_create_augroup("DprintAutoFormat", { clear = false })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = "DprintAutoFormat",
+        buffer = buf,
+        callback = run_dprint_format,
+      })
+      vim.notify("dprint autoformat enabled", vim.log.levels.INFO)
+    end
+  else
+    run_dprint_format()
+  end
+end, { desc = "Run dprint fmt on current buffer, or toggle autoformat with ':DprintFormat toggle'",
+  nargs = "?",
+   })
+
 return {
 	{'heavenshell/vim-jsdoc'}, -- run npm install in: nvim-data\lazy\vim-jsdoc\lib
 }
